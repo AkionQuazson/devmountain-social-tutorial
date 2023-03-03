3a661acc5571a62e5d243fbc1ad17ab599066f1a
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2023-03-02 18:45:35 -0700

    compleated

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -58,7 +58,16 @@ const editPost = async (req, res) => {
     }
 }
 const deletePost = async (req, res) => {
-
+    try {
+        const {id} = req.params;
+        await Post.destroy({
+            where: {id: +id}
+        })
+        res.sendStatus(200)
+    } catch (error) {
+        console.log('ERROR IN deletePost', error);
+        res.sendStatus(400);
+    }
 }
 module.exports = {
     getAllPosts,
